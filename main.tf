431075b1773cb799ced7bbdc8a62dd077c4b8a6d
Sachin <118503762+sachinpparmar@users.noreply.github.com>
2023-04-19 19:05:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -31,3 +31,37 @@ resource "aws_security_group" "Docker" {
     type = "terraform-test-security-group"
   }
 }
+------------------------------------
+  
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami             = "ami-06e46074ae430fba6"
+  instance_type   = "t2.micro"
+  #key_name        = "JainwindowsServer"
+  user_data       = file("file.sh")
+  security_groups = [aws_security_group.Docker.name]
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
+
+resource "aws_security_group" "Docker" {
+  description = "Security group for Docker containers"
+  tags = {
+    type = "terraform-test-security-group"
+  }
+}
